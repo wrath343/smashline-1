ae62e1430f69133a2dad9adab61e5bd956c1fe89
Wrath343 <bryan_m007@hotmail.com>
2024-01-23 23:28:57 -0600

    Add files via upload

---
+++ Bowser Down Air spike/src/lib.rs
@@ -0,0 +1,29 @@
+#![feature(
+    concat_idents,
+    proc_macro_hygiene
+)]
+#![allow(
+    unused_imports,
+    unused_parens,
+	unused_macros,
+	unused_variables,
+	unused_assignments,
+	non_upper_case_globals,
+	non_snake_case,
+	dead_code,
+    clippy::borrow_interior_mutable_const
+)]
+
+mod spikefixed; 
+
+
+
+
+#[skyline::main(name = "smashline_test")]
+pub fn main() {
+	spikefixed::install();	
+
+
+}
+
+
