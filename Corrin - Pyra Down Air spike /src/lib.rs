dcb78a17b8ea80c70e593b6c6b76382f306289e8
Wrath343 <bryan_m007@hotmail.com>
2024-01-24 22:28:35 -0600

    Add files via upload

---
+++ Corrin - Pyra Down Air spike /src/lib.rs
@@ -0,0 +1,25 @@
+#![feature(
+    concat_idents,
+    proc_macro_hygiene
+)]
+#![allow(
+    unused_imports,
+    unused_parens,
+	unused_macros,
+	unused_variables,
+	unused_assignments,
+	non_upper_case_globals,
+	non_snake_case,
+	dead_code,
+    clippy::borrow_interior_mutable_const
+)]
+
+mod spike;
+
+
+#[skyline::main(name = "smashline_test")]
+pub fn main() {
+	spike::install();
+}
+
+
