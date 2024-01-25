2932b1d8d741b1db1e15e0abeec098e7561e70d4
Wrath343 <bryan_m007@hotmail.com>
2024-01-24 20:50:57 -0600

    Add files via upload

---
+++ Capitan Falcon Ganondorf Punch/src/lib.rs
@@ -0,0 +1,24 @@
+#![feature(
+    concat_idents,
+    proc_macro_hygiene
+)]
+#![allow(
+    unused_imports,
+    unused_parens,
+	unused_macros,
+	unused_variables,
+	unused_assignments,
+	non_upper_case_globals,
+	non_snake_case,
+	dead_code,
+    clippy::borrow_interior_mutable_const
+)]
+
+mod falconpunch;
+mod faconpunchturn;
+
+#[skyline::main(name = "smashline_test")]
+pub fn main() {
+    falconpunch::install();
+    faconpunchturn::install();
+}
